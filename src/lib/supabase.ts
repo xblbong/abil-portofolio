34ed7fea70e60ade76243e0a50f5a7611b2ed231
xblbong <sblhh.m@gmail.com>
2026-02-03 17:44:03 +0700

    feat: lib/supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,11 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+
+// proteksi agar build tidak error jika variabel kosong
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.warn("Peringatan: Supabase URL atau Anon Key tidak ditemukan di environment variable.")
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
