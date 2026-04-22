d4e42781c637350d201721cc9f4cb78dfdc760fe
xblbong <sblhh.m@gmail.com>
2026-04-23 03:17:47 +0700

    fix: layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,6 +48,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-S8BZSHMF1Y"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-S8BZSHMF1Y');
+            `,
+          }}
+        />
+      </head>
       <body className={`${googleSans.variable} antialiased`}>
         <Providers>
           <PageLoader minDuration={1000} />
